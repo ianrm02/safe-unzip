49a7d0a2d50529a849b7bd6e0309f383670e3816
ianrm02 <ianmarchbank007@gmail.com>
2024-09-08 22:50:24 -0600

    Added readme and .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# safe-unzip
+**Safe UnZip**
+
+One of my students turned in a file that worried me. It turned out to be nothing, but I was inspired to write this and change how I open students zip files.
--- testUnzipSafe.sh
+++ testUnzipSafe.sh
@@ -1,14 +1,20 @@
 #!/bin/zsh
 
-# Test script to check the functionality of the zip bomb detector
+# With the added unzipping functionality, this script does not work as well
+
+# Consider adding a testing option to the script to fix that
 
 TEST_DIR="testZips"
-DETECTOR_SCRIPT="./unzipSafe.sh"
+DETECTOR_SCRIPT_N="./unzipSafe.sh"
+DETECTOR_SCRIPT_T="./unzipSafe.sh -t 2"
 
 # Loop through the test zip files
 for zipfile in "$TEST_DIR"/*.zip; do
   echo "--------------------------------"
-  echo "Testing file: $zipfile"
-  $DETECTOR_SCRIPT "$zipfile"
+  echo "Testing file: $zipfile (no options)"
+  $DETECTOR_SCRIPT_N "$zipfile"
   echo "--------------------------------"
+  echo "Testing file: $zipfile (-t 2)"
+  $DETECTOR_SCRIPT_T "$zipfile"
 done
+echo "--------------------------------"
